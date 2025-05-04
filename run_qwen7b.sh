678c6f0e81b8a030f6c34c9193ff4e446aafd6e9
wangshenglei <shenglei.wang@qq.com>
2025-05-04 17:05:36 +0800

    rm sh

--- run_qwen7b.sh
+++
@@ -1,9 +0,0 @@
-docker run -d --runtime nvidia --gpus all \
-    --name vllm_serve \
-    -v ./models:/models \
-    --net host \
-    harbor.prod.yxit.cc/vesta/vllm-openai:latest \
-    --model=/models/Qwen2.5-7B-Instruct \
-    --port=8102 \
-    --enable-auto-tool-choice \
-    --tool-call-parser hermes
